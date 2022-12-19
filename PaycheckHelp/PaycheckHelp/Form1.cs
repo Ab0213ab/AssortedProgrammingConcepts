747a6d061c83d20b93e30005da10e46bf07afbbb
Ab0213ab <aschaefer@my365.bellevue.edu>
2022-12-19 07:51:06 -0500

    Paycheck Helper added

---
+++ PaycheckHelp/PaycheckHelp/Form1.cs
@@ -0,0 +1,53 @@
+﻿// Andrew B. Schaefer
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaycheckHelp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            // MOVE TRY/CATCH TO METHOD?
+            try
+            {
+                double totalHours = double.Parse(totalHoursTextBox.Text);
+                double domesticPerDiemHours = double.Parse(domesticPerDiemTextBox.Text);
+                double internationalPerDiemHours = double.Parse(internationalPerDiemTextBox.Text);
+
+                double paycheck = Paycheck.calculatePaycheck(totalHours, domesticPerDiemHours,
+                                      internationalPerDiemHours);
+
+                paycheckAmountTextBox.Text += "~ $" + paycheck;
+            }
+
+            // Handles exceptions for both empty fields and invlaid input
+            catch (System.FormatException) 
+            {
+                paycheckAmountTextBox.Text += "All fields must be filled with numbers only!";
+            }
+        }
+
+        // Clears all form data
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+                totalHoursTextBox.Clear();
+                domesticPerDiemTextBox.Clear();
+                internationalPerDiemTextBox.Clear();
+                paycheckAmountTextBox.Clear();
+        }
+    }
+}
---
+++ PaycheckHelp/PaycheckHelp/Paycheck.cs
@@ -0,0 +1,34 @@
+﻿// Andrew B. Schaefer
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaycheckHelp
+{
+    internal class Paycheck
+    {
+        static double payRate = 40.16;
+        static double domesticPerDiemRate = 2.20;
+        static double internationalPerDiemRate = 2.50;
+        static double taxRate = .75;
+
+        // Calculates paycheck amount based on Paycheck class
+        // variables and user input
+        public static double calculatePaycheck(double totalHours, 
+                                               double domesticPerDiemHours,
+                                               double internationalPerDiemHours)
+        {
+            // CHANGE PREVENTER CODE SMELL? PERHAPS LONG METHOD?
+            // NEEDS EXTRACT VARIABLE?
+            double paycheckTotal = ((totalHours - 37.5) * payRate) * taxRate +
+            (domesticPerDiemHours * domesticPerDiemRate) +
+            (internationalPerDiemHours * internationalPerDiemRate);
+
+            return paycheckTotal;
+        }
+
+    }
+}
